d130d7bd15b8588da0d6a420ecdec93e9340693a
Mancaceresuade <162748921+Mancaceresuade@users.noreply.github.com>
2024-08-12 12:44:08 +0000

    maps

--- progiii/src/main/java/clase1/EjemploMaps.java
+++ progiii/src/main/java/clase1/EjemploMaps.java
@@ -1,6 +1,7 @@
 package clase1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EjemploMaps {
     public static void main(String[] args) {
@@ -9,11 +10,24 @@ public class EjemploMaps {
         facturas.add(new Factura(1,2,20000.0));
         facturas.add(new Factura(1,1,10000.0));
 
-        ArrayList<Cliente> clientes = new ArrayList<>();
-        clientes.add(new Cliente(1,"Carlos"));
-        clientes.add(new Cliente(2,"Juana"));
+        HashMap<Integer,Cliente> clientes = new HashMap<>();
+        clientes.put(1, new Cliente(1,"Carlos"));
+        clientes.put(2, new Cliente(2,"Juana"));
 
-        System.out.println("hola mundo");
+        HashMap<Integer, ResultadoDto> resultados = new HashMap<>();
+        for(Factura f : facturas) {
+            System.out.println(f.getIdCliente());
+            if(resultados.get(f.getIdCliente()) == null) {
+                String nombre = clientes.get(f.getIdCliente()).getNombre();
+                resultados.put(f.getIdCliente(), new ResultadoDto(f.getIdFactura(),nombre,0.0));                
+            }
+            double tot = resultados.get(f.getIdCliente()).getImporte() + f.getImporte();
+            resultados.get(f.getIdCliente()).setImporte(tot);
+        }
+        // imprimir el resultado
+        for (Integer id : resultados.keySet()) {
+            System.out.println(resultados.get(id));
+        }        
     }
 }
 
@@ -49,9 +63,37 @@ class Factura {
         return this.idFactrua;
     }
     public int getIdCliente() {
-        return this.idFactrua;
+        return this.idCliente;
     }
     public double getImporte() {
         return this.importe;
+    }
+}
+
+
+class ResultadoDto {
+    private int idCliente;
+    private String nombre;
+    private double importe;
+    ResultadoDto(int idCliente,String nombre, double importe) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.importe = importe;
+    }
+    public int getIdCliente() {
+        return this.idCliente;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public double getImporte() {
+        return this.importe;
+    }
+    void setImporte(double importe) {
+        this.importe = importe;
+    }
+    @Override
+    public String toString() {
+        return this.idCliente + " " + this.nombre + " " + this.importe;
     }
 }
